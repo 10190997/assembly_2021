ceced444358b33387e02e537430b1ef8aba59fda
10190997 <petrocollege10190997@gmail.com>
2021-09-18 12:11:36 +0300

    1709

--- 0709/0709.cpp
+++ 0709/0709.cpp
@@ -10,25 +10,24 @@ int main()
 	setlocale(LC_ALL, "rus");
 	short dim[] = { 1, -2, 3, -5, 4, 6, 7, 0, -10 };
     short res;
-    short l = 4;
+    short n = 9;
     _asm {
         xor ax, ax; 
      PREPARE: 
-        mov cx, 8; 
-        xor esi, esi;
-        lea esi, dim;
+        mov cx, n; 
+        xor ebx, ebx;
+        lea ebx, dim;
+        jcxz EXITER;
      COMPARE: 
-        mov dx, [esi]; 
+        mov dx, [ebx];
         cmp dx, 0; 
         jnge NEGAT; 
      POSIT: 
         add ax, dx; 
      NEGAT: 
-        inc esi; 
-        inc esi;
-        dec cx; 
-        jcxz EXITER; 
-        jmp COMPARE; 
+        inc ebx;
+        inc ebx;
+loop COMPARE; 
      EXITER: 
         mov res, ax
     }
